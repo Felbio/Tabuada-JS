d352bb75eb4afdda7fa9344d103a1e8d4f7a37b7
Felipe Oliveira <felipe.j.o.86@gmail.com>
2025-08-26 19:31:52 -0300

    creation of the css and javascript structures

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const multiplicationForm = document.querySelector("#mult-form");
+
+const numberInput = document.querySelector("#number");
+
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTitle = document.querySelector("#mult-title span");
+
+const multiplicationTable = document.querySelector("#mult-operations");
+
+//Funções
+const createTable = (number, multiplicatorNumber) => {
+	multiplicationTable.innerHTML = "";
+
+	for (let i = 1; i <= multiplicatorNumber; i++) {
+		const result = number * i;
+		const template = `<div class="row">
+        <div class="operation">${number} x ${i} =</div>
+        <div class="result">${result}</div>
+      </div>`;
+		const parser = new DOMParser();
+
+		const htmlTemplate = parser.parseFromString(template, "text/html");
+
+		const row = htmlTemplate.querySelector(".row");
+
+		multiplicationTable.appendChild(row);
+	}
+	multiplicationTitle.innerText = number;
+};
+
+// Eventos
+
+multiplicationForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const multiplicationNumber = +numberInput.value;
+
+	const multiplicatorNumber = +multiplicationInput.value;
+
+	if (!multiplicationNumber || !multiplicatorNumber) return;
+
+	createTable(multiplicationNumber, multiplicatorNumber);
+});
